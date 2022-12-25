189401bbaf487ad35c9d633f564f9e2bcd90b92e
jminango20 <jcarlosminango@gmail.com>
2022-12-25 17:19:54 -0300

    utils folder

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -19,6 +19,7 @@ const { deployments, network } = require("hardhat");
 //const helperConfig = require("../helper-hardhat-config")
 //const networkConfig = helperConfig.networkConfig
 const { networConfig, developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
 
 module.exports = async({getNamedAccounts, deployments}) => {
  const { deploy, log } = deployments
@@ -37,12 +38,19 @@ module.exports = async({getNamedAccounts, deployments}) => {
  }
 
 
+ const args = [ethsUsdPriceFeedAddress]
  //when going for localhost or hardhat network we want to use a mock
  const fundMe = await deploy("FundMe", {
     from: deployer,
-    args: [ethsUsdPriceFeedAddress], //put price feed address
+    args: args, //put price feed address
     log: true,
  })
+
+ if(!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY){
+   //verify
+   await verify(fundMe.address, args)
+ }
+
  log("---------------------------------------------------------------")
 }    
 module.exports.tags = ["all", "FundMe"]
